a69ed84cc2b63cdb1683626df416f979b49bd98c
Yulia Dobrova <dobrova2009@gmail.com>
2023-11-25 14:10:43 -0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # magic-ball
+
+A project practiced on the Web Development Intro Day
---
+++ script.js
@@ -0,0 +1,84 @@
+console.log("Have a good Saturday!");
+
+/**
+ * Here is our array of messages our 8ball will store!
+ *
+ * FIXME:
+ * Add all the possible fortunes we can get
+ * from the Magic 8-Ball!
+ */
+// const messages = ["FIXME", "FIXME"];
+const messages = [
+  "It is certain",
+  "It is decidedly so",
+  "I do not recommend",
+  "Don't even try",
+  "Bad idea",
+  "Great idea!",
+  "Yes, sure!",
+  "That sounds good",
+  "Yeah, it definitely needs to be done",
+  "I don't think it's a good idea",
+  "Maybe not today?",
+  "Definitely NO",
+  "You wish!",
+  "No doubt 😉",
+  "Absolutely",
+  "Possibly",
+  "Stars say NO",
+];
+
+/**
+ * Returns a random eight ball message
+ */
+function getEightBallMessage() {
+  // FIXME:
+  // Make a variable messageSize which gets the length
+  // of the array (or counts the amount of messages inside
+  // of that array)
+  let messageSize = messages.length;
+
+  // FIXME:
+  // Make a variable randomIndex, and set it to a random
+  // number from 0 to the length of the array.
+  let randomIndex = Math.floor(Math.random() * messageSize);
+
+  // FIXME:
+  // Get the randomIndexth message from our array!
+  // Let's call it fortuneStr.
+  let fortuneStr = messages[randomIndex];
+
+  return fortuneStr;
+}
+
+/**
+ * Takes an eightball message and places it inside
+ * the ball
+ */
+function changeMessage() {
+  // Use JavaScript to get the <div id="eight"> tag!
+  const eightEl = document.getElementById("eight");
+
+  // FIXME:
+  // What do you think we can do to use JS to get
+  // the <div id="answer"> tag? Name it answerEl.
+  const answerEl = document.getElementById("answer");
+
+  // This "clears" the eight ball message
+  eightEl.textContent = "";
+
+  // We want the 8Ball to pick a random fortune for us!
+
+  // FIXME:
+  // fill in the message with our fortune!
+  answerEl.textContent = getEightBallMessage();
+}
+
+// Use JavaScript to get the <div id="button"> tag!
+const buttonEl = document.getElementById("button");
+
+// Tell our browser to make sure the message
+// changes when we click on the "SHAKE" button.
+// "When we click on the button, let's run the
+// changeMessage function above!"
+buttonEl.addEventListener("click", changeMessage);
